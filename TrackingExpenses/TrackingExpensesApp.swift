2021446840129fc1d0f3812f1caccf25ff341bf5
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-10-10 19:01:50 +0200

    first commit

---
+++ TrackingExpenses/TrackingExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingExpensesApp.swift
+//  TrackingExpenses
+//
+//  Created by Влад on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackingExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
