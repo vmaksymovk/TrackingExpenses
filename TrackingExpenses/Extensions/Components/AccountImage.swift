734462cd293d54638db7fda2b7960480a79321ab
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-10-25 17:19:34 +0200

    Added possibility to changed accont picture

--- TrackingExpenses/Extensions/Components/AccountImage.swift
+++ TrackingExpenses/Extensions/Components/AccountImage.swift
@@ -1,22 +1,42 @@
-//
-//  AccountImage.swift
-//  TrackingExpenses
-//
-//  Created by Влад on 10/25/23.
-//
-
 import SwiftUI
+import RealmSwift
+
 
 struct AccountImage: View {
+    @State private var isImagePickerPresented = false // is libary with images appear
+    @State private var selectedImage: Image? // Choose image
+    
     var body: some View {
-        Image("sticker")
-            .resizable()
-            .frame(width: 140, height: 140)
-            .clipShape(Circle())
-            .overlay{
-                Circle().stroke(.gray, lineWidth: 4)
+        VStack {
+            if selectedImage != nil { // if user pick image
+                selectedImage!
+                    .resizable()
+                    .frame(width: 140, height: 140)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(Color.gray, lineWidth: 4)
+                    }
+                    .shadow(radius: 8)
+                    .onTapGesture {
+                        isImagePickerPresented = true
+                    }
+            } else { // if user didn't pick image
+                Image("sticker")
+                    .resizable()
+                    .frame(width: 140, height: 140)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(Color.gray, lineWidth: 4)
+                    }
+                    .shadow(radius: 8)
+                    .onTapGesture {
+                        isImagePickerPresented = true
+                    }
             }
-            .shadow(radius: 8)
+        }
+        .sheet(isPresented: $isImagePickerPresented) { // responsible for window with images(library)
+            ImagePicker(selectedImage: $selectedImage)
+        }
     }
 }
 
---
+++ TrackingExpenses/Extensions/Components/ImagePicker.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import RealmSwift
+
+struct ImagePicker: UIViewControllerRepresentable { // create a struct
+    @Binding var selectedImage: Image? // create a connection which image will pick user
+
+    func makeCoordinator() -> Coordinator { // method created for recognize which image picked user
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController { // method which responsible for editing picture which had been choose.
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = context.coordinator
+        imagePicker.allowsEditing = true
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {} // method which we can use in future for update contoller settings
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate { // method which responsible for cases choosing Image
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        
+        //method which transfer choose Image to Object Image, then selectedImage = Image
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.editedImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: uiImage)
+            }
+            picker.dismiss(animated: true)
+        }
+        
+        // In case User will close controller, this method deselect/dismiss user's choose.
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
+}
